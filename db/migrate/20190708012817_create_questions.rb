efeeac51bd9de34cb18915a2cb10a721380fce24
kumail <zdsd0019@gmail.com>
2019-07-10 02:08:17 -0400

    added db for form

---
+++ db/migrate/20190708012817_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :question do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :phone_number, null: false
+      t.text :body, null: false
+
+      t.timestamps  null: false
+    end
+  end
+end
